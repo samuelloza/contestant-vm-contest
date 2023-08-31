b1c3f494b7ee07ddcef847223bf69716ad1d8385
radl97 <radl97@users.noreply.github.com>
2023-08-31 12:00:17 +0200

    Add desktop entry for CMS website

--- sbin/mkioiuser.sh
+++ sbin/mkioiuser.sh
@@ -28,9 +28,12 @@ fi
 # Keyboard layout stuff
 mkdir /home/ioi/Desktop
 cp /usr/share/applications/gnome-keyboard-panel.desktop /home/ioi/Desktop
+cp /opt/ioi/misc/cms.desktop /home/ioi/Desktop
 chmod +x /home/ioi/Desktop/gnome-keyboard-panel.desktop
+chmod +x /home/ioi/Desktop/cms.desktop
 chown ioi.ioi -R /home/ioi/Desktop
 sudo -Hu ioi dbus-run-session gio set /home/ioi/Desktop/gnome-keyboard-panel.desktop metadata::trusted true
+sudo -Hu ioi dbus-run-session gio set /home/ioi/Desktop/cms.desktop metadata::trusted true
 sudo -Hu ioi dbus-run-session gsettings set org.gnome.shell.extensions.ding start-corner top-left
 
 # set default fullname and shell
